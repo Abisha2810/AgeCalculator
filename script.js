d884a01451633ba55753ff2b44a8569c852eb029
Abisha2810 <abisha102305@gmail.com>
2025-01-08 18:39:47 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function calculateAge() {
     alert("Please enter your birthday");
   } else {
     const age = getAge(birthdayValue);
-    resultEl.innerText = `Your age is ${age} ${age > 1 ? "years" : "year"} old`; // Fixed this line by adding backticks
+    resultEl.innerText = `Your age is ${age} ${age > 1 ? "years" : "year"} old`;  
   }
 }
 
